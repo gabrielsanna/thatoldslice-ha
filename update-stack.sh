d44459e0b04485a6835aaab7fe3d3390fa5e550d
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-12-16 12:11:46 -0500

    Added more steps and robust logic to orchestration scripts

--- update-stack.sh
+++ update-stack.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
-# A script to deploy the initial stack in Google Cloud and 
-# then apply Ansible playbooks to it.
+# A script to update the stack in Google Cloud and 
+# then reapply Ansible playbooks to it.
 #
 # Dependencies:
 # - ansible
 # - gcloud SDK
-# - mysql client
 #
 
 # Parameters
 STACK_NAME="thatoldslice-10"
 
 # Deploy the stack
-gcloud deployment-manager deployments create ${STACK_NAME} \
+gcloud deployment-manager deployments update ${STACK_NAME} \
 	--config ./deployment-manager/thatoldslice-infrastructure.yml
 
 if [ $? != 0 ]; then
-	echo "An error occurred deploying the stack."
-	exit 1
-fi
-
-# Populate database
-echo "Populating the database..."
-DB_IP=$(gcloud sql instances describe ${STACK_NAME}-instance | grep ipAddress: | awk -F':' '{print $2}' | awk '{print $1}')
-DB_NAME="${STACK_NAME}-db"
-DB_USER="${STACK_NAME}"
-
-echo "mysql -h ${DB_IP} -u ${DB_USER} -pdemopass1 ${DB_NAME} < data.sql"
-mysql -h ${DB_IP} -u ${DB_USER} -pdemopass1 ${DB_NAME} < data.sql
-
-if [ $? != 0 ]; then
-	echo "An error occurred populating the database."
+	echo "An error occurred updating the stack."
 	exit 1
 fi
 
 # Fix Django database config
 echo "Fixing Django settings..."
+DB_IP=$(gcloud sql instances describe ${STACK_NAME}-instance | grep ipAddress: | awk -F':' '{print $2}' | awk '{print $1}')
+DB_NAME="${STACK_NAME}-db"
+DB_USER="${STACK_NAME}"
+
 sed -i '' 's/DB_IP.*/DB_IP: '${DB_IP}'/g' ansible/thatoldslice/vars/main.yml
 sed -i '' 's/DB_NAME.*/DB_NAME: '${DB_NAME}'/g' ansible/thatoldslice/vars/main.yml
 sed -i '' 's/DB_USER.*/DB_USER: '${DB_USER}'/g' ansible/thatoldslice/vars/main.yml
